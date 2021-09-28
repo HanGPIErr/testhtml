56e5f044fa3fb15cba3e09c5ac99c1ecb7c0559f
PR LOPEZ <hangpierr@hotmail.fr>
2021-09-28 10:53:28 +0200

    10H53

---
+++ calculatrice.js
@@ -0,0 +1,25 @@
+function forclear() {
+    document.getElementById("rendu").innerHTML = "0";
+}
+function removeZero() {
+    var value = document.getElementById("rendu").innerHTML;
+    if (value == "0") {
+        value = " "
+        document.getElementById("rendu").innerHTML = value;
+    }
+}
+function perc() {
+    var value = document.getElementById("rendu").innerHTML;
+    value = value / 100;
+    document.getElementById("rendu").innerHTML = value;
+}
+function fordisplay(value) {
+    removeZero()
+    document.getElementById("rendu").innerHTML += value;
+}
+function resoudre() {
+    removeZero()
+    var equation = document.getElementById("rendu").innerHTML;
+    var solved = eval(equation);
+    document.getElementById('rendu').innerHTML = solved;
+}
